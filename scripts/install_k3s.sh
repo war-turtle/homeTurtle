cb0362d37708ef95222846f9f3a0b11a4880eec8
war-turtle <kartikyadav71199@gmail.com>
2022-01-15 11:21:08 +0530

    permission changed for kubeconfig

--- scripts/install_k3s.sh
+++ scripts/install_k3s.sh
@@ -1,7 +1,7 @@
 
 # downloading the k3s install script
-curl -sfL https://get.k3s.io -o /var/tmp/k3s
+curl -L https://get.k3s.io -o /var/tmp/k3s
 
 # running the downloaded script with kubeconfig 
 chmod +x /var/tmp/k3s
-K3S_KUBECONFIG_MODE="600" INSTALL_K3S_EXEC="server --disable=traefik" sh /var/tmp/k3s
+K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable=traefik" sh /var/tmp/k3s
